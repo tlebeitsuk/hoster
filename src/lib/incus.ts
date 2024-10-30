5037d883352dd56c3a2a62177990b4f5585a4bb7
Tobias Lebeitsuk <tobias.lebeitsuk@gmail.com>
2024-10-30 12:58:50 +0200

    list project servers

--- src/lib/incus.ts
+++ src/lib/incus.ts
@@ -1,23 +1,23 @@
-import { ofetch } from "ofetch";
-import { Agent } from "undici";
+import { ofetch } from "ofetch"
+import { Agent } from "undici"
 
 const secureAgent = new Agent({
-    connect: {
-        rejectUnauthorized: false,
-        key: process.env.INCUS_KEY,
-        cert: process.env.INCUS_CERT,
-    },
+  connect: {
+    rejectUnauthorized: false,
+    key: process.env.INCUS_KEY,
+    cert: process.env.INCUS_CERT,
+  },
 })
 
-export const useIncus = ofetch.create({
-    baseURL: `${process.env.INCUS_URL}/1.0`,
-    dispatcher: secureAgent,
-    keepalive: true,
-    headers:{
+export const callIncus = ofetch.create({
+  baseURL: `${process.env.INCUS_URL}/1.0`,
+  dispatcher: secureAgent,
+  keepalive: true,
+  headers: {
     "Content-Type": "application/json",
-    },
-    parseResponse: async (response) => {
-        const data = JSON.parse(response);
-        return data.metadata;
-    },
-});
+  },
+  parseResponse: async (response) => {
+    const data = JSON.parse(response)
+    return data.metadata
+  },
+})
