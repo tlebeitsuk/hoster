e6f1eac5ae60e6c6e8607019612287ac0009ab30
NatalieLindholm <natalie.lindholm05@gmail.com>
2024-11-13 12:00:04 +0200

    Added colors

--- src/components/sidebar-app.tsx
+++ src/components/sidebar-app.tsx
@@ -1,44 +1,44 @@
-'use client'
-import { Frame, Map, PieChart } from 'lucide-react'
-import User from '@/components/sidebar-user'
+"use client";
+import { Frame, Map, PieChart } from "lucide-react";
+import User from "@/components/sidebar-user";
 import {
-	Sidebar,
-	SidebarContent,
-	SidebarFooter,
-	SidebarRail,
-} from '@/components/ui/sidebar'
-import Projects from '@/components/sidebar-projects'
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarRail,
+} from "@/components/ui/sidebar";
+import Projects from "@/components/sidebar-projects";
 
 const data = {
-	projects: [
-		{
-			name: 'Project 1',
-			url: '/1',
-			icon: Frame,
-		},
-		{
-			name: 'Project 2',
-			url: '/2',
-			icon: PieChart,
-		},
-		{
-			name: 'Project 3',
-			url: '/3',
-			icon: Map,
-		},
-	],
-}
+  projects: [
+    {
+      name: "Project 1",
+      url: "/1",
+      icon: Frame,
+    },
+    {
+      name: "Project 2",
+      url: "/2",
+      icon: PieChart,
+    },
+    {
+      name: "Project 3",
+      url: "/3",
+      icon: Map,
+    },
+  ],
+};
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
-	return (
-		<Sidebar {...props}>
-			<SidebarContent>
-				<Projects projects={data.projects} />
-			</SidebarContent>
-			<SidebarFooter>
-				<User />
-			</SidebarFooter>
-			<SidebarRail />
-		</Sidebar>
-	)
+  return (
+    <Sidebar {...props} className="bg-sky-50">
+      <SidebarContent>
+        <Projects projects={data.projects} />
+      </SidebarContent>
+      <SidebarFooter>
+        <User />
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  );
 }
