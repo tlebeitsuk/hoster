d828f9b35b12c06ff7be7c16994c565d18e5458e
zekken <blch.namt@gmail.com>
2024-11-14 10:59:13 +0200

    merge conflicts

--- src/components/sidebar-projects.tsx
+++ src/components/sidebar-projects.tsx
@@ -32,7 +32,12 @@ export default function Projects({
 						</SidebarMenuButton>
 					</SidebarMenuItem>
 				))}
-				
+				<SidebarMenuItem>
+					<SidebarMenuButton className="text-sidebar-foreground/70">
+						<MoreHorizontal className="text-sidebar-foreground/70" />
+						<span>More</span>
+					</SidebarMenuButton>
+				</SidebarMenuItem>
 			</SidebarMenu>
 		</SidebarGroup>
 	)
