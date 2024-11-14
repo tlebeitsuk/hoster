246a5a840d32e427e8cfd4154e766ee4cb530e41
Philip Huynh <wuflerz@outlook.com>
2024-11-14 12:42:55 +0200

    Optimise prisma.ts

--- src/data/account/delete-account.ts
+++ src/data/account/delete-account.ts
@@ -1,7 +1,7 @@
 'use server'
 import { auth } from '@/lib/auth'
 import { headers } from 'next/headers'
-import { prisma } from '@/lib/prisma'
+import prisma from '@/lib/prisma'
 
 export async function deleteAccount() {
     try {
