3280ecd63b7e2f0f2bb1b7d57293a485480d10b3
zekken <blch.namt@gmail.com>
2024-11-20 13:44:46 +0200

    transfering files

--- src/app/dashboard/[projectId]/[serverName]/page.tsx
+++ src/app/dashboard/[projectId]/[serverName]/page.tsx
@@ -1,5 +1,13 @@
 import { getServers } from "@/data/projects/get-servers";
 import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+import {
   Table,
   TableBody,
   TableCell,
@@ -7,7 +15,24 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table"
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
 import { formatDistanceToNow } from "date-fns";
+import { Separator } from "@/components/ui/separator";
+import ServerStatusIcon from "@/components/server-status"
 
 type PageByIdProps = {
   params: {
@@ -19,29 +44,90 @@ type PageByIdProps = {
 export default async function InstancePage({ params }: PageByIdProps) {
   const { projectId, serverName } = params;
   const servers = await getServers(projectId);
-  
   const server = servers.find((s) => s.name === serverName);
+  const statusClass = server.status === "Running" ? "text-green-500" : "text-red-500";
 
   if (!server) {
     return <div>Server not found</div>;
   }
 
-  const statusClass = server.status === "Running" ? "text-green-500" : "text-red-500";
+  // const RAM = server.metadata
+  console.log(server);
+  
 
-  const createdAtResult = formatDistanceToNow(
-    server.created_at
-  ,{ addSuffix: true });
+  // const createdAtResult = formatDistanceToNow(
+  //   server.created_at
+  // ,{ addSuffix: true });
 
-  const usedAtResult = formatDistanceToNow(
-    server.last_used_at
-  ,{ addSuffix: true });
+  // const usedAtResult = formatDistanceToNow(
+  //   server.last_used_at
+  // ,{ addSuffix: true });
 
   return (
     <>
-      <div className="flex flex-wrap items-center flex-col pt-20 gap-5">
-        <h1 className="font-medium text-xl">{serverName}</h1>
-        <div className="mt-5 p-3">
-          <Table>
+      <Breadcrumb className="m-5">
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <BreadcrumbLink href="/dashboard">Dashboard</BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbLink href={"/dashboard/" + projectId}>Project {projectId}</BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbPage>{serverName}</BreadcrumbPage>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+
+      <div className="flex flex-wrap m-5 flex-col gap-5">
+        <div className="flex gap-2">
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger>
+                <ServerStatusIcon params={statusClass} />
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Server is {server.status}.</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+          <h1 className="font-medium text-xl">{serverName}</h1>
+        </div>
+        
+        <Separator />
+
+        <div className="mt-5 p-3 flex gap-5">
+          <Card className="w-[350px]">
+            <CardHeader>
+              <CardTitle>Memory Usage <span className="text-green-500">[RAM]</span></CardTitle>
+              <CardDescription>31%/100%</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p>Total capacity: 16 GB</p>
+              <p>Used capacity: 16 GB</p>
+            </CardContent>
+            <CardFooter className="flex justify-between">
+              
+            </CardFooter>
+          </Card>
+
+          <Card className="w-[350px]">
+            <CardHeader>
+              <CardTitle> <span className="text-blue-500">[Disk]</span> Storage</CardTitle>
+              <CardDescription>31%/100%</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p>Total capacity: 16 GB</p>
+              <p>Used capacity: 16 GB</p>
+            </CardContent>
+            <CardFooter className="flex justify-between">
+              
+            </CardFooter>
+          </Card>
+
+          {/* <Table>
             <TableHeader>
               <TableRow>
                 <TableHead className="w-[200px]">Status</TableHead>
@@ -60,8 +146,9 @@ export default async function InstancePage({ params }: PageByIdProps) {
                 <TableCell>{server.type}</TableCell>
               </TableRow>
             </TableBody>
-          </Table>
+          </Table> */}
         </div>
+
       </div>
     </>
   );
