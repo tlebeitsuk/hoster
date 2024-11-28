c4e0b574c78e93a619b2a50b9bd04e22f2c171a9
Philip Huynh <wuflerz@outlook.com>
2024-11-28 10:48:44 +0200

    fix

--- src/data/projects/get-servers.ts
+++ src/data/projects/get-servers.ts
@@ -1,17 +1,8 @@
 import { callIncus } from "@/lib/incus";
 
 export async function getServers(projectId?: string) {
-  const servers = await callIncus("/instances?recursion=2", {
+  const servers = await callIncus("/instances", {
     query: { project: projectId, recursion: 2 },
   });
-
-  return servers;
-}
-
-export async function getProjects(projectId?: string) {
-  const servers = await callIncus("/instances", {
-    query: { project: projectId, recursion: 1 },
-  });
-
   return servers;
 }
