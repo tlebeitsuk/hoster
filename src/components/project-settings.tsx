40f59c9ad170f37a604ea53ebae866db187aa9e2
Philip Huynh <wuflerz@outlook.com>
2024-12-10 13:14:46 +0200

    Merge branch 'main' into alex-change-project-description

--- src/components/project-settings.tsx
+++ src/components/project-settings.tsx
@@ -17,8 +17,11 @@ import {
 } from '@/components/ui/alert-dialog'
 import { Separator } from '@/components/ui/separator'
 import { useRouter } from 'next/navigation'
+import {
+  changeProjectDescription,
+  renameProject,
+} from '@/data/projects/project-settings'
 import { Textarea } from './ui/textarea'
-import { changeProjectDescription, renameProject } from '@/data/projects/rename-project'
 
 export default function Component({ params }) {
   const [newName, setNewName] = useState('')
@@ -27,42 +30,45 @@ export default function Component({ params }) {
   const [errorMessage, setErrorMessage] = useState('')
   const [successMessage, setSuccessMessage] = useState('')
   const router = useRouter()
-  
+
   const projects = params.projects
   const projectID = params.projectID
   const thisProject = projects.find((p) => p.id === Number(projectID))
-  
-  console.log(thisProject);
+
+  console.log(thisProject)
 
   const handleClick = async () => {
-    if (!newName.trim() && !newDescription.trim()) { 
+    if (!newName.trim() && !newDescription.trim()) {
       setErrorMessage('There are no changes to be saved.')
-      return;
+      return
     }
 
     setIsSaving(true)
     setErrorMessage('')
     setSuccessMessage('')
-    
-    try {
-      if(newName.trim() && newDescription.trim()){
-        const changedName = await renameProject(Number(projectID), newName)
-        const changedDescription = await changeProjectDescription(Number(projectID), newDescription)
-        setSuccessMessage('Updated successfully!')
 
-      } else if(newName.trim()) {
+    try {
+      if (newName.trim() && newDescription.trim()) {
+        const changedName = await renameProject(Number(projectID), newName)
+        const changedDescription = await changeProjectDescription(
+          Number(projectID),
+          newDescription
+        )
+        setSuccessMessage('Updated successfully!')
+      } else if (newName.trim()) {
         const changes = await renameProject(Number(projectID), newName)
         setSuccessMessage('Updated project!')
-
-      } else if(newDescription.trim()) {
-        const changes = await changeProjectDescription(Number(projectID), newDescription)
+      } else if (newDescription.trim()) {
+        const changes = await changeProjectDescription(
+          Number(projectID),
+          newDescription
+        )
         setSuccessMessage('Updated successfully!')
       }
 
       setNewName('')
       setNewDescription('')
       router.refresh()
-
     } catch (error: any) {
       setErrorMessage(error.message || 'Failed to save changes.')
     } finally {
@@ -76,83 +82,87 @@ export default function Component({ params }) {
         <CardTitle className="text-2xl">Project Settings</CardTitle>
       </CardHeader>
       <CardContent className="space-y-6">
-       <div className="flex flex-col gap-4">
-        <div className="group relative flex-grow">
-          <label
-            htmlFor="projectName"
-            className="origin-start absolute top-1/2 block -translate-y-1/2 cursor-text px-1 text-sm text-muted-foreground/70 transition-all group-focus-within:pointer-events-none group-focus-within:top-0 group-focus-within:cursor-default group-focus-within:text-xs group-focus-within:font-medium group-focus-within:text-foreground has-[+input:not(:placeholder-shown)]:pointer-events-none has-[+input:not(:placeholder-shown)]:top-0 has-[+input:not(:placeholder-shown)]:cursor-default has-[+input:not(:placeholder-shown)]:text-xs has-[+input:not(:placeholder-shown)]:font-medium has-[+input:not(:placeholder-shown)]:text-foreground"
+        <div className="flex flex-col gap-4">
+          <div className="group relative flex-grow">
+            <label
+              htmlFor="projectName"
+              className="origin-start absolute top-1/2 block -translate-y-1/2 cursor-text px-1 text-sm text-muted-foreground/70 transition-all group-focus-within:pointer-events-none group-focus-within:top-0 group-focus-within:cursor-default group-focus-within:text-xs group-focus-within:font-medium group-focus-within:text-foreground has-[+input:not(:placeholder-shown)]:pointer-events-none has-[+input:not(:placeholder-shown)]:top-0 has-[+input:not(:placeholder-shown)]:cursor-default has-[+input:not(:placeholder-shown)]:text-xs has-[+input:not(:placeholder-shown)]:font-medium has-[+input:not(:placeholder-shown)]:text-foreground"
+            >
+              <span className="inline-flex bg-background px-2">
+                Project Name
+              </span>
+            </label>
+            <Input
+              id="projectName"
+              type="text"
+              value={newName}
+              onChange={(e) => setNewName(e.target.value)}
+              placeholder=""
+              className="w-full"
+            />
+          </div>
+          <div className="group relative flex-grow">
+            <label
+              htmlFor="projectDescription"
+              className="origin-start absolute top-1/2 block -translate-y-1/2 cursor-text px-1 text-sm text-muted-foreground/70 transition-all group-focus-within:pointer-events-none group-focus-within:top-0 group-focus-within:cursor-default group-focus-within:text-xs group-focus-within:font-medium group-focus-within:text-foreground has-[+input:not(:placeholder-shown)]:pointer-events-none has-[+input:not(:placeholder-shown)]:top-0 has-[+input:not(:placeholder-shown)]:cursor-default has-[+input:not(:placeholder-shown)]:text-xs has-[+input:not(:placeholder-shown)]:font-medium has-[+input:not(:placeholder-shown)]:text-foreground"
+            >
+              <span className="inline-flex bg-background px-2">
+                Description
+              </span>
+            </label>
+            <Textarea
+              id="projectDescription"
+              value={newDescription}
+              onChange={(e) => setNewDescription(e.target.value)}
+              placeholder=""
+              className="w-full"
+            />
+          </div>
+          {successMessage && (
+            <p className="text-green-500 text-sm mt-2">{successMessage}</p>
+          )}
+          <Button
+            onClick={handleClick}
+            disabled={isSaving}
+            className="self-start"
           >
-            <span className="inline-flex bg-background px-2">
-              Project Name
-            </span>
-          </label>
-          <Input
-            id="projectName"
-            type="text"
-            value={newName}
-            onChange={(e) => setNewName(e.target.value)}
-            placeholder=""
-            className="w-full"
-          />
+            <Save className="mr-2 size-4" />
+            {isSaving ? 'Saving...' : 'Save Changes'}
+          </Button>
         </div>
-        <div className="group relative flex-grow">
-          <label
-            htmlFor="projectDescription"
-            className="origin-start absolute top-1/2 block -translate-y-1/2 cursor-text px-1 text-sm text-muted-foreground/70 transition-all group-focus-within:pointer-events-none group-focus-within:top-0 group-focus-within:cursor-default group-focus-within:text-xs group-focus-within:font-medium group-focus-within:text-foreground has-[+input:not(:placeholder-shown)]:pointer-events-none has-[+input:not(:placeholder-shown)]:top-0 has-[+input:not(:placeholder-shown)]:cursor-default has-[+input:not(:placeholder-shown)]:text-xs has-[+input:not(:placeholder-shown)]:font-medium has-[+input:not(:placeholder-shown)]:text-foreground"
-          >
-            <span className="inline-flex bg-background px-2">
-              Description
-            </span>
-          </label>
-          <Textarea
-            id="projectDescription"
-            value={newDescription}
-            onChange={(e) => setNewDescription(e.target.value)}
-            placeholder=""
-            className="w-full"
-          />
-        </div>
-        {successMessage && (
-        <p className="text-green-500 text-sm mt-2">{successMessage}</p>
+        {errorMessage && (
+          <p className="text-red-500 text-sm mt-2">{errorMessage}</p>
         )}
-        <Button onClick={handleClick} disabled={isSaving} className='self-start'>
-          <Save className="mr-2 size-4" />
-          {isSaving ? 'Saving...' : 'Save Changes'}
-        </Button>
-      </div>
-      {errorMessage && (
-        <p className="text-red-500 text-sm mt-2">{errorMessage}</p>
-      )}
-      <Separator />
-      <div>
-        <h3 className="text-lg font-semibold mb-2">Remove Project</h3>
-        <AlertDialog>
-          <AlertDialogTrigger asChild>
-            <Button variant="destructive">
-              <Trash2 className="mr-2 size-4" />
-              Permanently Delete
-            </Button>
-          </AlertDialogTrigger>
-          <AlertDialogContent>
-            <AlertDialogHeader>
-              <AlertDialogTitle>
-                Are you sure you want to delete this project?
-              </AlertDialogTitle>
-              <AlertDialogDescription>
-                This action cannot be undone. This will permanently delete
-                your project and remove all of its contents from our servers.
-              </AlertDialogDescription>
-            </AlertDialogHeader>
-            <AlertDialogFooter>
-              <AlertDialogCancel>Cancel</AlertDialogCancel>
-              <Button variant="destructive" asChild>
-                <AlertDialogAction>Confirm</AlertDialogAction>
+        <Separator />
+        <div>
+          <h3 className="text-lg font-semibold mb-2">Remove Project</h3>
+          <AlertDialog>
+            <AlertDialogTrigger asChild>
+              <Button variant="destructive">
+                <Trash2 className="mr-2 size-4" />
+                Permanently Delete
               </Button>
-            </AlertDialogFooter>
-          </AlertDialogContent>
-        </AlertDialog>
-      </div>
-    </CardContent>
-  </Card>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <AlertDialogTitle>
+                  Are you sure you want to delete this project?
+                </AlertDialogTitle>
+                <AlertDialogDescription>
+                  This action cannot be undone. This will permanently delete
+                  your project and remove all of its contents from our servers.
+                </AlertDialogDescription>
+              </AlertDialogHeader>
+              <AlertDialogFooter>
+                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                <Button variant="destructive" asChild>
+                  <AlertDialogAction>Confirm</AlertDialogAction>
+                </Button>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
+        </div>
+      </CardContent>
+    </Card>
   )
 }
