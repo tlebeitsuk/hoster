1c67e769add2f7512a604c886e0c5e787158fbfd
Philip Huynh <wuflerz@outlook.com>
2024-12-19 09:59:30 +0200

    Check if there is servers in project before deleting

--- src/app/dashboard/[projectId]/settings/page.tsx
+++ src/app/dashboard/[projectId]/settings/page.tsx
@@ -1,9 +1,21 @@
 import ProjectSettings from '@/components/project-settings'
 import { getProjects } from '@/data/projects/get-projects'
+import { getServers } from '@/data/projects/get-servers'
 
-export default async function SettingsPage(query: { params: { projectId: string } }) {
-  const projects = await getProjects();
-  const projectID = query.params.projectId  
+export default async function SettingsPage({
+  params,
+}: {
+  params: Promise<{ projectId: string }>
+}) {
+  const projectID = (await params).projectId
+  const projects = await getProjects(Number(projectID))
+  const servers = await getServers(projectID.toString())
 
-  return <ProjectSettings params={{projects, projectID}} />
+  return (
+    <ProjectSettings
+      projects={projects as Project}
+      projectID={projectID}
+      servers={servers as Server[]}
+    />
+  )
 }
--- src/app/types.d.ts
+++ src/app/types.d.ts
@@ -5,3 +5,13 @@ type Project = {
     createdAt: Date
     updatedAt: Date
 }
+
+type Server = {
+    id: string
+    name: string
+    status: ServerStatus
+    created_at: Date
+    last_used_at: Date
+    state?: ServerState
+    projectId: string
+}
